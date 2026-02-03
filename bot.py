6e4aaa752a0c4912011f46674a0e126622d8ac46
smiley-94 <sandu.flavius@icloud.com>
2026-02-03 17:16:46 +0100

    Refactored `bot.py`: improved environment variable handling, added HTTPX request configuration with custom timeouts.

--- bot.py
+++ bot.py
@@ -1,38 +1,70 @@
 #!/usr/bin/env python3
 import logging
+import os
 
 try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except Exception:
-    pass
+	from dotenv import load_dotenv
+except ModuleNotFoundError:
+	pass
+else:
+	load_dotenv()
 
-from telegram.ext import ApplicationBuilder, Application
+from telegram.ext import ApplicationBuilder, Application, ContextTypes
 from telegram import Update
+from telegram.request import HTTPXRequest
+from i18n import loadLang
 import db
 from handlers import registerHandlers
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_ADMIN_ID, TELEGRAM_ALLOWED_USERS, APP_LOG_LEVEL, APP_DB_PATH
 
-# Set up logging
-logLevel = getattr(logging, APP_LOG_LEVEL)
-logging.basicConfig(level=logLevel, format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+logLevel = getattr(logging, os.environ["logLevel"])
+logging.basicConfig(
+	level=logLevel,
+	format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
 
-async def onStartup(app: Application):
-    from i18n import loadLang
-    loadLang("lang.json")
-    await db.ensureSchema()
-    for uid in TELEGRAM_ALLOWED_USERS:
-        await db.allowUser(uid)
+logger = logging.getLogger(__name__)
+
+botToken = os.environ["botToken"]
+adminUserId = os.environ["adminUserId"]
+
+async def onStartup(_: Application):
+	loadLang("lang.json")
+	await db.ensureSchema()
+
+async def errorHandler(_: object, context: ContextTypes.DEFAULT_TYPE):
+	logger.error("Exception while handling an update:", exc_info=context.error)
 
 def main():
-    app = (
-        ApplicationBuilder()
-        .token(TELEGRAM_BOT_TOKEN)
-        .post_init(onStartup)
-        .build()
-    )
-    registerHandlers(app, TELEGRAM_ADMIN_ID)
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+	request = HTTPXRequest(
+		connect_timeout=60.0,
+		read_timeout=75.0,
+		write_timeout=60.0,
+		pool_timeout=60.0,
+		connection_pool_size=8
+	)
+
+	app = (
+		ApplicationBuilder()
+		.token(botToken)
+		.request(request)
+		.get_updates_request(request)
+		.post_init(onStartup)
+		.build()
+	)
+
+	app.add_error_handler(errorHandler)
+
+	registerHandlers(
+		app,
+		int(adminUserId) if adminUserId and adminUserId.isdigit() else None
+	)
+
+	app.run_polling(
+		allowed_updates=Update.ALL_TYPES,
+		drop_pending_updates=True,
+		timeout=60,
+		bootstrap_retries=-1
+	)
 
 if __name__ == "__main__":
-    main()
+	main()
